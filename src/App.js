5823864bd1c29453a1666c1ea99a2ba5dfa16feb
geetansh810 <agrawalgeetansh810@gmail.com>
2022-06-26 20:58:07 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './imgs/icons8-coding-49.png';
 import './App.css';
 
 function App() {
--- src/core/Footer.js
+++ src/core/Footer.js
@@ -6,10 +6,12 @@ const Footer = () => {
   return (
     <footer className="footer bg-dark mt-auto">
       <div className="container-fluid nav-color text-white text-center py-3">
-      <div className="slogan">
-            A one stop solution for all Academic Needs.
-          </div>
-        <div className="slogan">If you got any questions, feel free to reach out!!!</div>
+        <div className="slogan">
+          A one stop solution for all Academic Needs.
+        </div>
+        <div className="slogan">
+          If you got any questions, feel free to reach out!!!
+        </div>
         <a
           className="btn btn-primary btn-md"
           href="mailto:geetanshagrawal810@gmail.com"
@@ -21,21 +23,10 @@ const Footer = () => {
       </div>
       <div className="container text-center py-2">
         <div className="row">
-          <div className="col-md-8 text-muted">
-            Made by{" "}
-            <span className="text-white">
-              <a
-                href="https://www.linkedin.com/in/geetansh-agrawal-1b8a27190/"
-                className="text-decoration-none text-white fw-bold"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Geetansh Agrawal - Arthraj Rathore
-              </a>
-            </span>{" "}
-            in MERN Stack
+          <div className="col-md-3 text-muted text-end">
+            Made by
           </div>
-          <div className="col-md-4 d-flex text-light gap-4 justify-content-center align-items-center">
+          <div className="col-md-6 d-flex text-light gap-4 justify-content-center align-items-center">
             <div>
               <a
                 className="d-flex align-items-center gap-3 text-decoration-none text-light fw-bold"
@@ -43,11 +34,14 @@ const Footer = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <span>Github</span>
-                {/* <img className="social-logo" src={github} alt="" /> */}
+                <img
+                  className="social-logo bg-white rounded-circle"
+                  src="https://cdn-icons-png.flaticon.com/512/733/733609.png"
+                  alt=""
+                />
               </a>
             </div>
-            |
+            Geetansh Agrawal
             <div className="d-flex align-items-center gap-3">
               <a
                 className="d-flex align-items-center gap-3 text-decoration-none text-light fw-bold"
@@ -55,10 +49,46 @@ const Footer = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <span>LinkenIN</span>
-                {/* <img className="social-logo" src={linkedin} alt="" /> */}
+                <img
+                  className="social-logo"
+                  src="https://cdn-icons-png.flaticon.com/512/145/145807.png"
+                  alt=""
+                />
               </a>
             </div>
+            <div> -------- </div>
+            <div>
+              <a
+                className="d-flex align-items-center gap-3 text-decoration-none text-light fw-bold"
+                href="https://github.com/arthraj"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="social-logo bg-white rounded-circle"
+                  src="https://cdn-icons-png.flaticon.com/512/733/733609.png"
+                  alt=""
+                />
+              </a>
+            </div>
+            Arthraj Rathore
+            <div className="d-flex align-items-center gap-3">
+              <a
+                className="d-flex align-items-center gap-3 text-decoration-none text-light fw-bold"
+                href="https://www.linkedin.com/in/arthraj-rathore/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="social-logo"
+                  src="https://cdn-icons-png.flaticon.com/512/145/145807.png"
+                  alt=""
+                />
+              </a>
+            </div>
+          </div>
+          <div className="col-md-3 text-muted text-start">
+            in MERN Stack
           </div>
         </div>
       </div>
